17dbfe1bd89c939145c0353696456320ac7a487d
gudebin <gudebin@outlook.com>
2023-05-21 19:04:25 +0800

    update database-access

---
+++ database-access/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User:   "root",
+		Passwd: "123456",
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "db_example",
+	}
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
